83acb77d1d9a3b58463f11b66e9a2dbabcbd79db
jaryarbn <grandline501@gmail.com>
2022-03-16 03:19:25 +0000

    Update at 2022-03-16T03:19:25.888082Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,15 +2,15 @@ package com.bytelegend;
 
 public class Main {
     public static byte int2byte(int i) {
-        return (byte)i;
+        return (byte) i;
     }
 
     public static short int2short(int i) {
-        return (short)i;
+        return (short) i;
     }
 
     public static char int2char(int i) {
-        return (char)i;
+        return (char) i;
     }
 
     // Cast an int to String, e.g. 123 -> "123"
