a0b1d716e7c9dae773f9326f337921d45babc70e
wangjie-fourth <wangjie_fourth@163.com>
2022-03-19 14:54:13 +0000

    Update at 2022-03-19T14:54:13.898244Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,15 +2,15 @@ package com.bytelegend;
 
 public class Main {
     public static byte int2byte(int i) {
-        return (byte) i / 2;
+        return (byte) i;
     }
 
     public static short int2short(int i) {
-        return (short) i / 2;
+        return (short) i;
     }
 
     public static char int2char(int i) {
-        return (char) i / 2;
+        return (char) i;
     }
 
     // Cast an int to String, e.g. 123 -> "123"
@@ -20,19 +20,19 @@ public class Main {
 
     // Cast a String to int, e.g. "123" -> 123
     public static int string2int(String s) {
-        return Integer.valueOf(s);
+        return Integer.parseInt(s);
     }
 
     // Cast a String to double, e.g. "1.23" -> 1.23
     public static double string2double(String s) {
-        return Double.valueOf(s);
+        return Double.parseDouble(s);
     }
 
     public static int castLong2int(Long l) {
-        return (int) l;
+        return Math.toIntExact(l);
     }
 
     public static long double2Long(Double d) {
-        return (long) d;
+        return Long.parseLong(String.valueOf(d));
     }
 }
