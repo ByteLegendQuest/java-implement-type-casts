bab414bbcebb58edda76d858bf33520d246ce650
hupizhu <2815695640@qq.com>
2022-04-01 13:01:00 +0000

    Update at 2022-04-01T13:01:00.438052Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -20,19 +20,19 @@ public class Main {
 
     // Cast a String to int, e.g. "123" -> 123
     public static int string2int(String s) {
-        return Integer.valueOf(s).intvalue();
+        return Integer.valueOf(s).intValue();
     }
 
     // Cast a String to double, e.g. "1.23" -> 1.23
     public static double string2double(String s) {
-        return Double.valueOf(s).doublevalue();
+        return Double.valueOf(s).doubleValue();
     }
 
     public static int castLong2int(Long l) {
-        return (int) l;
+        return l;
     }
 
     public static long double2Long(Double d) {
-        return (long) d;
+        return d;
     }
 }
