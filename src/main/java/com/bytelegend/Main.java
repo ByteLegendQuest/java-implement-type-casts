72860516b42a237698d47a90bf8745ced9c132a1
g497813927 <admin@techzjc.com>
2022-04-15 08:11:24 +0000

    Update at 2022-04-15T08:11:24.342882Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,38 +1,36 @@
-package com.bytelegend;
-
 public class Main {
     public static byte int2byte(int i) {
-        return i;
+        return (byte) i;
     }
 
     public static short int2short(int i) {
-        return i;
+        return (short)i;
     }
 
     public static char int2char(int i) {
-        return i;
+        return (char)i;
     }
 
     // Cast an int to String, e.g. 123 -> "123"
     public static String int2String(int i) {
-        return Strng.valueOf(i);
+        return String.valueOf(i);
     }
 
     // Cast a String to int, e.g. "123" -> 123
     public static int string2int(String s) {
-        return Integer.parseInt(s);
+        return Integer.valueOf(s);
     }
 
     // Cast a String to double, e.g. "1.23" -> 1.23
     public static double string2double(String s) {
-        return Double.parseDouble(s);
+        return Double.valueOf(s);
     }
 
     public static int castLong2int(Long l) {
-        return (int) l;
+        return l.intValue();
     }
 
     public static long double2Long(Double d) {
-        return (long) d;
+        return d.intValue();
     }
 }
