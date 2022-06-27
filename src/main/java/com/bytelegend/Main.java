2447bfd4b470d28ff787e0abe94bfd4c97dc0d49
batman405 <2638603343@qq.com>
2022-06-27 03:47:37 +0000

    Update at 2022-06-27T03:47:37.462668Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -15,24 +15,24 @@ public class Main {
 
     // Cast an int to String, e.g. 123 -> "123"
     public static String int2String(int i) {
-        return (String) i;
+        return int2String(i);
     }
 
     // Cast a String to int, e.g. "123" -> 123
     public static int string2int(String s) {
-        return (int) s;
+        return string2int(s);
     }
 
     // Cast a String to double, e.g. "1.23" -> 1.23
     public static double string2double(String s) {
-        return (double) s;
+        return string2double(s);
     }
 
     public static int castLong2int(Long l) {
-        return (int) l;
+        return castLong2int(l);
     }
 
     public static long double2Long(Double d) {
-        return (long) d;
+        return double2Long(d);
     }
 }
