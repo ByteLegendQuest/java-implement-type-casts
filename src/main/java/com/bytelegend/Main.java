525a61012332f60306a1719472521f7b8df4ebce
Birlinha <omenaribeiro13@gmail.com>
2022-07-14 18:05:18 +0000

    Update at 2022-07-14T18:05:18.354631Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -11,24 +11,24 @@ public class Main {
 
     // Cast an int to String, e.g. 123 -> "123"
     public static String int2String(int i) {
-        return String.valueOf(i);
+        return String i;
     }
 
     // Cast a String to int, e.g. "123" -> 123
     public static int string2int(String s) {
-        return Integer.parseInt(s);
+        return (int) s;
     }
 
     // Cast a String to double, e.g. "1.23" -> 1.23
     public static double string2double(String s) {
-        return Double.parseDouble(s);
+        return (double) s;
     }
 
     public static int castLong2int(Long l) {
-        return Math.toIntExact(l);
+        return (int) l;
     }
 
     public static long double2Long(Double d) {
-        return Long.valueOf(String.valueOf(d));
+        return Long d;
     }
 }
