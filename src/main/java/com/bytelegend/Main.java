8eeaa9d187173a9ffd74c3858a889ac20a89da80
tassellll <2112562745@qq.com>
2022-07-25 13:12:13 +0000

    Update at 2022-07-25T13:12:13.762558Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,37 +2,37 @@ package com.bytelegend;
 
 public class Main {
     public static byte int2byte(int i) {
-        return (byte)i;
+        return (byte) i;
     }
 
     public static short int2short(int i) {
-        return (short)i;
+        return (short) i;
     }
 
     public static char int2char(int i) {
-        return (char)i;
+        return (char) i;
     }
 
     // Cast an int to String, e.g. 123 -> "123"
     public static String int2String(int i) {
-        return (String)i;
+        return String.valueOf(i);
     }
 
-    // Cast a String to int, e.g. "123" -> 123
+    // Cast an String to int, e.g. "123" -> 123
     public static int string2int(String s) {
-        return (int)s;
+        return Integer.parseInt(s);
     }
 
-    // Cast a String to double, e.g. "1.23" -> 1.23
+    // Cast an String to double, e.g. "1.23" -> 1.23
     public static double string2double(String s) {
-        return (double)s;
+        return Double.parseDouble(s);
     }
 
     public static int castLong2int(Long l) {
-        return (int)l;
+        return (int) l.longValue();
     }
 
     public static long double2Long(Double d) {
-        return (Long)d;
+        return (long) d.doubleValue();
     }
 }
