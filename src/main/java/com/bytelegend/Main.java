e59076cb813414e98e30cc53615928d93d3a5b6a
tiger0961 <29515547@qq.com>
2022-10-12 06:14:13 +0000

    Update at 2022-10-12T06:14:13.132048Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,15 +2,15 @@ package com.bytelegend;
 
 public class Main {
     public static byte int2byte(int i) {
-        return (byte)(i & 0xFF);;
+        return (byte) (i & 0xFF);
     }
 
     public static short int2short(int i) {
-        return (short)i;
+        return (short) i;
     }
 
     public static char int2char(int i) {
-        return (char) (i+'0');
+        return (char) (i + '0');
     }
 
     // Cast an int to String, e.g. 123 -> "123"
@@ -20,7 +20,7 @@ public class Main {
 
     // Cast a String to int, e.g. "123" -> 123
     public static int string2int(String s) {
-        return Integer.valueOf(s).intValue();;
+        return Integer.valueOf(s).intValue();
     }
 
     // Cast a String to double, e.g. "1.23" -> 1.23
@@ -33,6 +33,6 @@ public class Main {
     }
 
     public static long double2Long(Double d) {
-        return Double(d).longValue();;
+        return new Double(d).longValue();;
     }
 }
