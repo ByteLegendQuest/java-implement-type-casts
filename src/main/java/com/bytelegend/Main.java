6517d3582a12aaed88e13c23217376f1858a3b8d
tiger0961 <29515547@qq.com>
2022-10-12 05:57:16 +0000

    Update at 2022-10-12T05:57:16.795258Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -15,17 +15,17 @@ public class Main {
 
     // Cast an int to String, e.g. 123 -> "123"
     public static String int2String(int i) {
-        return i;
+        return i.toSring();
     }
 
     // Cast a String to int, e.g. "123" -> 123
     public static int string2int(String s) {
-        return s;
+        return Integer.parseInt(s);
     }
 
     // Cast a String to double, e.g. "1.23" -> 1.23
     public static double string2double(String s) {
-        return s;
+        return Double.parseDouble(s);
     }
 
     public static int castLong2int(Long l) {
